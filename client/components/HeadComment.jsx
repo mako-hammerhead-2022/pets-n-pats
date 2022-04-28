bb163e7e67170c4f54528825b93d6ccf4be18b25
charlie-r-gray <charliegreatpaint@gmail.com>
2022-04-29 11:21:08 +1200

    sstart user feedback on submit

--- client/components/HeadComment.jsx
+++ client/components/HeadComment.jsx
@@ -10,16 +10,37 @@ import {
   Button,
   FormControl,
   FormLabel,
-  Input
+  Input,
+  useToast,
+  Portal
 } from '@chakra-ui/react'
 
 import { postComment } from '../apiClient/comments'
 
+  {/* This component is now a reusable module (not just for Head)
+      and expects an object prop that looks like this:
+
+       <Component ani={{id: #, name: ""}} />   */}
+
+
 export function HeadComment({ ani }) {
 
-  if(!ani) return null
+ // toast response handling
+// const toast = useToast()
+// const breadSlice = {
+//       title: 'Comment added',
+//       description: `Your comment on ${ani.name} has been added.`,
+//       status: 'success',
+//       duration: 9000,
+//       isClosable: true,
+//     }
 
+    // local state
   const [comment, setComment] = useState('')
+  const [loadCheck, setLoadCheck] = useState(false)
+  const [buttonFill, setButtonFill] = useState('Submit')
+
+  // popoverform handling
 
   function handleChange(e) {
     e.preventDefault()
@@ -27,36 +48,50 @@ export function HeadComment({ ani }) {
   }
 
   function handleSave(e) {
+    setLoadCheck(true)
     const commentObj = {
-      petId: e.target.petId,
+      petId: ani.id,
+      authorId: "auth0|something",
       content: comment
     }
     e.preventDefault()
     postComment(commentObj)
+    .then(()=>{
+      setLoadCheck(false)
+      setButtonFill('Added!')
+      // button reponse value changes are
+    })
+    // .then(()=>toast(breadSlice))
+    .catch((err)=>{console.log(err.message)})
   }
 
+  // prevent render errors on initial load
+  if(!ani) return null
+
+  // screen render
   return (
     <>
-    <Popover>
+    <Popover  closeDelay={1500}>
       <PopoverTrigger>
         <Button>Add Comment</Button>
       </PopoverTrigger>
+      <Portal>
       <PopoverContent>
         <PopoverArrow />
         <PopoverCloseButton zIndex="6"/>
         <PopoverBody>
           <FormControl>
               <FormLabel>{`Comment on ${ani.name}`}</FormLabel>
-              <Input type="hidden" name='petId' value={ani.id} />
               <Input onChange={handleChange} placeholder={`What do you think of ${ani.name}?`} value={comment} />
             </FormControl>
             </PopoverBody>
             <PopoverFooter>
-          <Button onClick={handleSave} colorScheme='blue' mr={3}>
-              Submit
+          <Button onClick={handleSave} colorScheme='blue' mr={3} isLoading={loadCheck}>
+              {buttonFill}
           </Button>
           </PopoverFooter>
       </PopoverContent>
+      </Portal>
     </Popover>
     </>
   )
