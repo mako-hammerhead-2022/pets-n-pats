f8d69ce8686724899244110dfa693e388636bb68
Anna An <annaan.golf@gmail.com>
2022-04-28 20:52:23 +1200

    Refactor s3 apiClient to use superagent instead of fetch

--- client/apiClient/s3.js
+++ client/apiClient/s3.js
@@ -1,25 +1,18 @@
-
-// const baseUrl = 'http://localhost:3001'
+import request from 'superagent'
 
 export async function getImageUrl(file) {
-  const requestObject = {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      fileName: file.name,
-      fileType: file.type
-    })
+  const fileObject = {
+    fileName: file.name,
+    fileType: file.type
   }
+  const { signedUrl } = await request
+    .post('/api/image')
+    .send(fileObject)
+    .then(res => res.body)
 
-  const res = await fetch('/api/image', requestObject)
-  const { signedUrl } = await res.json()
-
-  await fetch(signedUrl, {
-    method: 'PUT',
-    body: file
-  })
-  const imageUrl = signedUrl.split('?')[0];
-  return imageUrl;
+  return request.put(signedUrl)
+    .send(file)
+    .then(() => {
+      return signedUrl.split('?')[0];
+    })
 }
