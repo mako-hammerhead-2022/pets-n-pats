54fed2702b9983f22d25c9bf388081a231f7c911
Anna An <annaan.golf@gmail.com>
2022-04-28 16:12:18 +1200

    Complete PetForm component with chakra

--- client/components/PetForm.jsx
+++ client/components/PetForm.jsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react'
 import api from '../apiClient'
 import { useAuth0 } from '@auth0/auth0-react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormLabel,
+  Input,
+  Textarea,
+  Select,
+  useDisclosure,
+  FormControl,
+  Container,
+} from '@chakra-ui/react'
+import { Form, Formik } from 'formik'
 
 const PetForm = () => {
   const [name, setName] = useState('')
   const [bio, setBio] = useState('')
-  const [animal, setAnimal] = useState('')
+  const [animal, setAnimal] = useState('cat')
   const [selectedFile, setSelectedFile] = useState(null)
 
-  const { user } = useAuth0()
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
-  const handleFileChange = (e) => { 
+  const { isAuthenticated, user } = useAuth0()
+
+  const handleFileChange = (e) => {
     setSelectedFile(e.target.files[0])
   }
 
@@ -21,34 +40,96 @@ const PetForm = () => {
       name,
       bio,
       animal,
-      imageUrl
+      imageUrl,
     }
     await api.addPet(formData)
+
+    onClose()
   }
 
   return (
     <>
-      <label htmlFor="name">Name:</label>
-      <input
-        type="text"
-        id="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <label htmlFor="bio">Bio:</label>
-      <textarea
-        id="bio"
-        value={bio}
-        onChange={(e) => setBio(e.target.value)}
-      />
-      <label htmlFor="animal">They are a:</label>
-      <select name="animal" id="animal" value={animal} onChange={(e) => setAnimal(e.target.value)}>
-        <option value="cat">Cat</option>
-        <option value="dog">Dog</option>
-      </select>
-      <label htmlFor="image">Upload image:</label>
-      <input type="file" name="image" id="image" accept="image/*" onChange={handleFileChange}/>
-      <button onClick={handleSubmit}>Add Pet</button>
+      {isAuthenticated && (
+        <Button onClick={onOpen} colorScheme="teal">
+          + Add Pet
+        </Button>
+      )}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add Pet</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Formik
+              initialValues={{}}
+              onSubmit={ async (values, actions) => {
+                await actions.setSubmitting(false)
+                handleSubmit()
+              }}
+            >
+              {(props) => (
+                <Form>
+                  <FormControl>
+                    <FormLabel htmlFor="name">Name:</FormLabel>
+                    <Input
+                      type="text"
+                      id="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                    />
+                    <FormLabel htmlFor="bio">Bio:</FormLabel>
+                    <Textarea
+                      type="text"
+                      id="bio"
+                      value={bio}
+                      onChange={(e) => setBio(e.target.value)}
+                      required
+                    />
+                    <FormLabel htmlFor="animal">
+                      Select type of animal:
+                    </FormLabel>
+                    <Select
+                      name="animal"
+                      id="animal"
+                      value={animal}
+                      onChange={(e) => setAnimal(e.target.value)}
+                      required
+                    >
+                      <option value="cat">Cat</option>
+                      <option value="dog">Dog</option>
+                    </Select>
+                    <FormLabel htmlFor="image">Upload image:</FormLabel>
+                    <Input
+                      type="file"
+                      name="image"
+                      id="image"
+                      accept="image/*"
+                      onChange={handleFileChange}
+                      required
+                    />
+                  </FormControl>
+                  <Container centerContent>
+                    <div>
+                      <Button
+                        type="submit"
+                        isLoading={props.isSubmitting}
+                        colorScheme="teal"
+                        m={2}
+                      >
+                        Add Pet
+                      </Button>
+                      <Button onClick={onClose} m={2}>
+                        Cancel
+                      </Button>
+                    </div>
+                  </Container>
+                </Form>
+              )}
+            </Formik>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
