58e395b47750c0d38415ff0cadb695450a71cf39
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-04-28 18:42:22 +1200

    Remove test-related TODO and commented code

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -10,15 +10,7 @@ function App() {
   // TODO hard-coded for HeadToHead - replace with UseSelector() for deployment
   //----------------------------------------------
   const { cat, dog } = objTwoPet
-  //-----------------------------------------------------
-
-  // TODO: adjust tests to wrap components that depend on redux in a fakeStore
-  // const dispatch = useDispatch()
-  // const pets = useSelector((state) => state.pets)
-
-  // useEffect(() => {
-  //   dispatch(fetchTwoPets())
-  // }, [])
+  //----------------------------------------------
 
   return (
     <div>
---
+++ client/reducers/__tests__/pets.test.js
@@ -0,0 +1,18 @@
+import pets from '../index'
+import { arrTwoPet, objTwoPet } from '../../../__mockdata__/mockPetData'
+
+describe('pets Reducer', () => {
+  it('receives two random pets, one cat, one dog', () => {
+    const action = {
+      type: 'RECEIVE_RANDOM_TWO_PETS',
+      payload: objTwoPet,
+    }
+    const inputState = []
+    const expectedOutputState = arrTwoPet
+    const outputState = pets(inputState, action)
+
+    expect(outputState).toEqual(expectedOutputState)
+    // assert that we haven't 'mutated' our state
+    expect(outputState).not.toBe(inputState)
+  })
+})
