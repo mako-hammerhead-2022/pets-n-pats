885aa6abcbe6858546a3b12d491cd1de1f538698
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2022-04-30 11:25:22 +1200

    box - inherit full width of container

--- client/components/MyPets.jsx
+++ client/components/MyPets.jsx
@@ -33,7 +33,7 @@ function MyPets() {
   const renderedPets = pets?.map((pet) => {
     return (
       <WrapItem w='200px' minH='300px' key={pet.id} bg='gray.100'>
-        <Box p={2}>
+        <Box p={2} width='full'>
           <AspectRatio maxW='200px' ratio={4 / 3} mb={2}>
             <Image
               objectFit='cover'
