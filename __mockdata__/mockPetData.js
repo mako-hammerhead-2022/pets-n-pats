32f8932c3446c12301f3d6957a3584ed077d024d
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2022-04-29 15:11:25 +1200

    merge pr

--- __mockdata__/mockPetData.js
+++ __mockdata__/mockPetData.js
@@ -1,26 +1,27 @@
-export const arrTwoPet = [{
-  id: 3,
-  userId: 'auth0|something',
-  name: 'Giralda',
-  bio: 'Customizable holistic conglomeration',
-  imageUrl: 'https://cdn2.thecatapi.com/images/MTg0NjE0OQ.jpg',
-  animal: 'cat',
-  points: 17,
-  createdAt: '2022-04-26 22:28:54',
-  updatedAt: '2022-04-26 22:28:54',
-},
-{
-  id: 4,
-  userId: 'auth0|something',
-  name: 'Letizia',
-  bio: 'Open-architected systemic groupware',
-  imageUrl: 'https://images.dog.ceo/breeds/tervuren/yoda_in_garden.jpg',
-  animal: 'dog',
-  points: 83,
-  createdAt: '2022-04-26 22:28:54',
-  updatedAt: '2022-04-26 22:28:54',
-}]
-
+export const arrTwoPet = [
+  {
+    id: 3,
+    userId: 'auth0|something',
+    name: 'Giralda',
+    bio: 'Customizable holistic conglomeration',
+    imageUrl: 'https://cdn2.thecatapi.com/images/MTg0NjE0OQ.jpg',
+    animal: 'cat',
+    points: 17,
+    createdAt: '2022-04-26 22:28:54',
+    updatedAt: '2022-04-26 22:28:54',
+  },
+  {
+    id: 4,
+    userId: 'auth0|something',
+    name: 'Letizia',
+    bio: 'Open-architected systemic groupware',
+    imageUrl: 'https://images.dog.ceo/breeds/tervuren/yoda_in_garden.jpg',
+    animal: 'dog',
+    points: 83,
+    createdAt: '2022-04-26 22:28:54',
+    updatedAt: '2022-04-26 22:28:54',
+  },
+]
 
 export const objTwoPet = {
   cat: {
@@ -38,7 +39,7 @@ export const objTwoPet = {
     id: 5,
     userID: 'auth0|something',
     name: 'The Balrog',
-    bio: 'I\'ll have those on my own desk by five',
+    bio: "I'll have those on my own desk by five",
     imageUrl: 'https://images.dog.ceo/breeds/tervuren/yoda_in_garden.jpg',
     animal: 'dog',
     points: 420,
@@ -74,8 +75,10 @@ export const apiClientPet = {
   bio: 'random bio',
   animal: 'cat',
   imageUrl: 'image.jpg',
-export const objComment ={
+}
+
+export const objComment = {
   petId: 4,
   authId: 'auth0|something',
-  content: 'this pet comment is for petId4, he big'
+  content: 'this pet comment is for petId4, he big',
 }
--- client/apiClient/index.js
+++ client/apiClient/index.js
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 // import * as exampleApi from './examplePath'
 import * as s3Api from './s3'
 import * as petsApi from './pets'
-=======
 import * as commentApi from './comments'
->>>>>>> main
 import * as votesApi from './votes'
 
 export default {
-<<<<<<< HEAD
   ...s3Api,
   ...petsApi,
-=======
   ...commentApi,
->>>>>>> main
   ...votesApi,
 }
- 
