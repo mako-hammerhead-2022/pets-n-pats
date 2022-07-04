42e94eb7024a0ca0f92585ae835fe26e2c195758
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2022-07-04 22:59:56 +1200

    fix test

--- client/App.test.js
+++ client/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import Login from '@/components/Login.jsx'
 import App from '@/App.jsx'
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+
+import Login from '@/components/Login.jsx'
+import Home from '@/pages/Home.jsx'
 
 import { objTwoPet } from '~/test/fake-data'
 
@@ -22,18 +24,21 @@ const fakeStore = {
 }
 
 jest.mock('@/components/Login.jsx')
+jest.mock('@/pages/Home.jsx')
 
 describe('<App />', () => {
   it('renders the Login component', () => {
     Login.mockReturnValue(<div>Login Component</div>)
+    Home.mockReturnValue(<div>Home Component</div>)
     render(
       <Provider store={fakeStore}>
-        <Router>
+        <Router initialEntries={['/']}>
           <App />
         </Router>
       </Provider>
     )
     const loginComponent = screen.getByText('Login Component')
     expect(loginComponent).toBeInTheDocument()
+    expect(screen.getByText('Home Component')).toBeInTheDocument()
   })
 })
