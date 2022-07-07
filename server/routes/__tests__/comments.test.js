862676d60da9f1ffcabd9c0763112ffd4d7c26a1
dhdaveidhd <46812457+dhdaveidhd@users.noreply.github.com>
2022-07-07 21:44:43 +1200

    fix tests on comments.test.js under client/apiClient, and comments.test.js under server/routes

--- server/routes/__tests__/comments.test.js
+++ server/routes/__tests__/comments.test.js
@@ -3,12 +3,28 @@ const server = require('../../server')
 const db = require('../../db')
 
 import { objComment } from '~/test/fake-data'
+const { checkJwt } = require('../../utils')
 
 jest.mock('../../db')
+jest.mock('../../utils')
+
+beforeAll(() => {
+  jest.spyOn(console, 'log')
+  console.log.mockImplementation(() => {})
+  checkJwt.mockImplementation((req, res, next) => {
+    next()
+  })
+})
+
+afterAll(() => {
+  console.log.mockRestore()
+  jest.restoreAllMocks()
+})
 
 describe('POST /api/comments', () => {
   it('adding a comment hits the db', () => {
     db.insertComment.mockReturnValue(Promise.resolve(objComment))
+
     return request(server)
       .post('/api/comments')
       .send(objComment)
@@ -16,6 +32,7 @@ describe('POST /api/comments', () => {
         expect(res.status).toBe(200)
       })
   })
+
   it("should return status 500 and error when database doesn't work", () => {
     expect.assertions(2)
     db.insertComment.mockImplementation(() =>
@@ -23,6 +40,7 @@ describe('POST /api/comments', () => {
     )
     return request(server)
       .post('/api/comments')
+      .send(objComment)
       .then((res) => {
         expect(res.status).toBe(500)
         expect(res.text).toContain('Something went wrong')
