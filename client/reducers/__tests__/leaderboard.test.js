ad5150f1de2091c1f9ef91d8706d4459f5b8ac17
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-07-07 16:50:09 +1200

    wrote tests for error and request reducer

--- client/reducers/__tests__/leaderboard.test.js
+++ client/reducers/__tests__/leaderboard.test.js
@@ -1,9 +1,13 @@
-import { leaderboard_receiveData } from '@/actions'
+import {
+  leaderboard_receiveData,
+  leaderboard_setError,
+  leaderboard_requestData,
+} from '@/actions'
 import leaderboardReducer from '@/reducers/leaderboard'
 import { petsWithScores } from '~/test/fake-data'
 
 describe('top 10 pets reducer', () => {
-  test('receives 10 top scoring pets', () => {
+  it('receives 10 top scoring pets', () => {
     const action = {
       type: leaderboard_receiveData,
       leaderboard: petsWithScores,
@@ -14,5 +18,27 @@ describe('top 10 pets reducer', () => {
 
     expect(actualOutputState.leaderboard).toEqual(expectedOutputState)
     expect(actualOutputState).not.toBe(inputState)
+  })
+
+  it('sends an error', () => {
+    const action = {
+      type: leaderboard_setError,
+      errMessage: 'There is an error!',
+    }
+    const inputState = { data: [], error: null, loading: false }
+    const expectedOutputState = 'There is an error!'
+    const actualOutputState = leaderboardReducer(inputState, action)
+
+    expect(actualOutputState.error).toEqual(expectedOutputState)
+  })
+
+  it('sets loading to true on request data', () => {
+    const action = {
+      type: leaderboard_requestData,
+    }
+    const inputState = { data: [], error: null, loading: false }
+    const actualOutputState = leaderboardReducer(inputState, action)
+
+    expect(actualOutputState.loading).toBe(true)
   })
 })
