4261a1ef7a5e0d077508583437ad391b6ab2d7cc
charlotte-greatbatch <103108567+charlotte-greatbatch@users.noreply.github.com>
2022-07-07 15:40:08 +1200

    test postVoteTie passed

--- client/apiClient/__tests__/votes.test.js
+++ client/apiClient/__tests__/votes.test.js
@@ -1,33 +1,29 @@
 import nock from 'nock'
-import { postVotes } from '@/apiClient'
+import { postVotes, postVotesTie } from '@/apiClient'
 import { arrTwoPet } from '~/test/fake-data'
 
 describe('postVotes', () => {
-  test('calls /api/v1/votes/add with the correct params', async () => {
+  test('calls /api/votes/add with the correct params', async () => {
     const scope = nock('http://localhost')
       .patch('/api/votes/add')
       .reply(200, arrTwoPet[1])
 
     const response = await postVotes(arrTwoPet[1])
-    console.log(response)
 
-    expect(response.points).toBe(arrTwoPet[1].points)
+    expect(response).toStrictEqual(arrTwoPet[1])
     scope.done()
   })
 })
 
 describe('postVotesTie', () => {
-  it.todo('calls /api/votes/addTie with the correct params')
+  test('calls /api/votes/addTie with the correct params', async () => {
+    const scope = nock('http://localhost')
+      .patch('/api/votes/addTie')
+      .reply(200, arrTwoPet[0], arrTwoPet[1])
+
+    const response = await postVotesTie(arrTwoPet[0], arrTwoPet[1])
+
+    expect(response).toStrictEqual(arrTwoPet[0], arrTwoPet[1])
+    scope.done()
+  })
 })
-
-// describe('POST /api/comments', () => {
-//   test('post request to api route to add comment to db', async () => {
-//     const scope = nock('http://localhost')
-//       .post('/api/comments')
-//       .reply(200, objComment)
-
-//     const commentRes = await postComment(objComment)
-//     expect(commentRes.content).toBe(objComment.content)
-//     scope.done()
-//   })
-// })
